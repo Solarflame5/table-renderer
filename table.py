e222be1e074224a8ff3f55ce31178060957587bf
Solarflame5 <47642904+Solarflame5@users.noreply.github.com>
2023-04-27 16:28:29 +0300

    add column width calculation

--- table.py
+++ table.py
@@ -1,9 +1,16 @@
 import json
 
+def calculate_column_width(column):
+	input_column = column
+	input_column["contents"].insert(0, input_column["title"])
+	width = max([len(x) for x in input_column["contents"]])
+	return width
+
 def generate_table(table_json: str):
 	table_dict = json.loads(table_json)
-	return table_dict
-	
+	calculate_column_width(table_dict["columns"][0][0])
+	return
+
 # temporary code for testing
 from pathlib import Path
 json_path = Path(__file__).with_name("placeholder.json")
